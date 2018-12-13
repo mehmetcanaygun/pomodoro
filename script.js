d1c026c78526ab66c69fec64537ad9b512c896dc
Can <mca199667@gmail.com>
2018-12-14 01:01:31 +0300

    Pause and Restart buttons

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 var currentGoal;
+var inputBox = document.getElementById('input-box');
+var goalInput = document.getElementById('goal-input');
 var checkSuccessBox = document.getElementById('check-success-box');
 var question = document.getElementById('question');
 var goalList = document.getElementById('goal-list');
 var currentListItem = "";
 var goalListData = "";
 var countdown;
+var successfulPom = 0;
+var unsuccessfulPom = 0;
+var successRateLine = document.getElementById('success-rate');
+var successRate = 0;
+var isPaused = false;
 
 //Timer variables
 var minutes = 00;
 var seconds = 02;
 
 var saveGoalBtn = document.getElementById('save-goal-btn').onclick = function() {
-  var goalInput = document.getElementById('goal-input').value;
-  if(goalInput == "") {
+  if(goalInput.value == "") {
     alert("Enter a goal!");
   } else {
-    currentGoal = goalInput;
+    currentGoal = goalInput.value;
   }
+  inputBox.style.display = "none";
 }
 
 var startBtn = document.getElementById('start-btn').onclick = function() {
+  isPaused = false;
   startTimer();
 }
 
@@ -53,17 +61,38 @@ function startCountdown() {
   }
 }
 
+var pauseBtn = document.getElementById('pause-btn').onclick = function() {
+  if(isPaused == false) {
+    isPaused = true;
+    clearInterval(countdown);
+  }
+}
+
+var restartBtn = document.getElementById('restart-btn').onclick = function() {
+  reset();
+  goalList.innerHTML = "";
+  goalListData = "";
+  successfulPom = 0;
+  unsuccessfulPom = 0;
+  successRate = 0;
+  successRateLine.innerHTML = "";
+}
+
 function endPomodoro() {
   // 1 Pomodoro over, ask if it was successful or not
   checkSuccessBox.style.display = "block";
   question.innerHTML = "Did you reach your goal?<br>" +
                         "(" + currentGoal + ")";
+
 }
 
 var yesBtn = document.getElementById('yes-btn').onclick = function() {
   currentListItem = "<li>" + currentGoal + " <span class='successful'>DONE</span></li><br>";
   goalListData += currentListItem;
   goalList.innerHTML = goalListData;
+  question.innerHTML = "";
+  checkSuccessBox.style.display = "none";
+  successfulPom++;
   reset();
 }
 
@@ -71,13 +100,20 @@ var noBtn = document.getElementById('no-btn').onclick = function() {
   currentListItem = "<li>" + currentGoal + " <span class='unsuccessful'>FAILED</span></li><br>";
   goalListData += currentListItem;
   goalList.innerHTML = goalListData;
+  question.innerHTML = "";
+  checkSuccessBox.style.display = "none";
+  unsuccessfulPom++;
   reset();
 }
 
 function reset() {
+  goalInput.value = "";
+  inputBox.style.display = "block";
   currentGoal = "";
   currentListItem = "";
   minutes = 00;
   seconds = 02;
+  successRate = ((successfulPom/(successfulPom+unsuccessfulPom))*100).toFixed(2);
+  successRateLine.innerHTML = successRate + "%";
   clearInterval(countdown);
 }
